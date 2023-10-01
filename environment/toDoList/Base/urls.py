e701aeafa8b1afcf6052014b1ea20e430f7b9d76
JoseGonzCSE <jgonzalez437@ucmerced.edu>
2023-09-30 20:54:07 -0700

    Update 2:
    Added:
    View Task
    Add Task
    Edit Task
    Delete Task- Delete confirmation
    Back Functionality

--- environment/toDoList/Base/urls.py
+++ environment/toDoList/Base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TaskList,DetailTask
+from .views import TaskList,DetailTask,TaskCreate,TaskUpdate,TaskDelete
 
 urlpatterns=[
     path('',TaskList.as_view(),name='Tasks'),
     path('task/<int:pk>/',DetailTask.as_view(),name='Detail'),
-
+    path('create-Task/',TaskCreate.as_view(),name='Create'),
+    path('task-Update/<int:pk>/',TaskUpdate.as_view(),name='Update'),
+    path('task-Delete/<int:pk>/',TaskDelete.as_view(),name='Delete'),
 ]
--- environment/toDoList/Base/views.py
+++ environment/toDoList/Base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -10,3 +12,18 @@ class TaskList(ListView):
 class DetailTask(DetailView):
     model=Task
     context_object_name="Detail"
+
+class TaskCreate(CreateView):
+    model= Task
+    fields='__all__'
+    success_url=reverse_lazy('Tasks')
+
+class TaskUpdate(UpdateView):
+    model=Task
+    fields='__all__'
+    success_url=reverse_lazy('Tasks')
+
+class TaskDelete(DeleteView):
+    model=Task
+    context_object_name="Tasks"
+    success_url=reverse_lazy('Tasks')
